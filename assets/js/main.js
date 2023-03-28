1537edbe5c8474c3b019d93fbef0409dcba789f8
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-28 18:27:52 +0200

    Inserimento dinamico delle immagini con js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,30 @@
 CONSEGNA:
 Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
 */
+
+// creo l'array di immagini
+
+let imgArray =[
+    `<img id="img-slider" style="height: 30rem;" src="./assets/img/01.webp" alt="gatto.webp">`,
+    `<img id="img-slider" style="height: 30rem;" src="./assets/img/02.webp" alt="gatto.webp">`,
+    `<img id="img-slider" style="height: 30rem;" src="./assets/img/03.webp" alt="gatto.webp">`,
+    `<img id="img-slider" style="height: 30rem;" src="./assets/img/04.webp" alt="gatto.webp">`,
+    `<img id="img-slider" style="height: 30rem;" src="./assets/img/05.webp" alt="gatto.webp">`
+]
+
+console.log(imgArray)
+
+// inserisco dinamicamente le immagini con js
+document.getElementById("slider").innerHTML += `
+    <div class="item first active">
+        ${imgArray[0]}
+    </div>
+`;
+
+for(let i = 1; i < 5; i++){
+    document.getElementById("slider").innerHTML +=`
+    <div class="item">
+        ${imgArray[i]}
+    </div>
+    `
+}
